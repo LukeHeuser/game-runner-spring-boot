417078a75fb5d45363ede19e4b0992f53958ff0f
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2025-01-05 18:00:41 -0500

    Third Commit

--- src/main/java/com/heuser/learn_spring_framwork/AppGaming.java
+++ src/main/java/com/heuser/learn_spring_framwork/AppGaming.java
@@ -1,15 +1,17 @@
 package com.heuser.learn_spring_framwork;
 
 import com.heuser.learn_spring_framwork.game.GameRunner;
-import com.heuser.learn_spring_framwork.game.SuperContraGame;
+import com.heuser.learn_spring_framwork.game.RideFive;
 
 public class AppGaming {
 
     public static void main(String[] args) {
 
-        // Created a new 'marioGame' of the returned object type from MarioGame
+
         //var game = new MarioGame();
-        var game = new SuperContraGame();
+        //var game = new SuperContraGame();
+        var game = new RideFive();
+
         var gameRunner = new GameRunner(game);
         gameRunner.run();
 
---
+++ src/main/java/com/heuser/learn_spring_framwork/game/RideFive.java
@@ -0,0 +1,20 @@
+package com.heuser.learn_spring_framwork.game;
+
+public class RideFive implements GameConsole{
+
+    public void up() {
+        System.out.println("Shift up a gear");
+
+    }
+    public void down() {
+        System.out.println("Shift down a gear");
+    }
+    public void left() {
+        System.out.println("Lean Left");
+    }
+    public void right() {
+        System.out.println("Lean Right");
+    }
+
+
+}
