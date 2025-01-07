8d1d9180649b428c6a160d9aa45ab919c378f061
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2025-01-06 21:34:51 -0500

    Fifth Commit

--- src/main/java/com/heuser/learn_spring_framwork/AppGaming.java
+++ src/main/java/com/heuser/learn_spring_framwork/AppGaming.java
@@ -9,9 +9,12 @@ public class AppGaming {
 
         //var game = new MarioGame();
         //var game = new SuperContraGame();
-        var game = new RideFive();
+        var game = new RideFive(); //1: Object Creation
 
         var gameRunner = new GameRunner(game);
+            //2: Object Creation + Wiring of Dependency
+            // Game is a Dependency of GameRunner
+
         gameRunner.run();
 
     }
