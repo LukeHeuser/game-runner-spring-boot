0ce1c78220831ff6c694028b4b83318207a3df36
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2025-01-06 21:30:49 -0500

    Fourth Commit

--- src/main/java/com/heuser/learn_spring_framwork/AppGaming.java
+++ src/main/java/com/heuser/learn_spring_framwork/AppGaming.java
@@ -7,14 +7,12 @@ public class AppGaming {
 
     public static void main(String[] args) {
 
-
         //var game = new MarioGame();
         //var game = new SuperContraGame();
         var game = new RideFive();
 
         var gameRunner = new GameRunner(game);
         gameRunner.run();
-
 
     }
 
