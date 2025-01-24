24da9f868ad5ea353e3927d12d4f03c9b7b723ac
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2025-01-23 23:00:50 -0500

    Eighth  Commit

--- src/main/java/com/heuser/learn_spring_framwork/HelloWorldConfiguration.java
+++ src/main/java/com/heuser/learn_spring_framwork/HelloWorldConfiguration.java
@@ -3,6 +3,10 @@ package com.heuser.learn_spring_framwork;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// These records will print the to string method when called
+record Person (String name, int age) { };
+record Address (String streetName, String city) { };
+
 @Configuration
 public class HelloWorldConfiguration {
 
@@ -15,5 +19,16 @@ public class HelloWorldConfiguration {
     public int age() {
         return 25;
     }
+
+    @Bean
+    public Person person() {
+        return new Person("Hannah", 20);
+    }
+
+    @Bean
+    public Address address() {
+        return new Address("Sandusky St.", "Delaware");
+    }
+
 
 }
